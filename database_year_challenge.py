2f5c4e9dc28f709494baced6300020611203f66a
AndrewGluss <90129910+AndrewGluss@users.noreply.github.com>
2024-10-14 22:03:09 +0300

    added select_user and update_user functions

--- database_year_challenge.py
+++ database_year_challenge.py
@@ -1,33 +1,53 @@
 import sqlite3
 import time
+from year_challenge import YearChallenge
 
-async def create_user(user, user_challenge):
+
+def create_user(user, user_challenge: YearChallenge):
     connection = sqlite3.connect('year_challenge.db')
 
     cursor = connection.cursor()
-    x = time.time()
-    
+
     # Подумать нужно ли сделать id или username уникальным
+    # Добавить needTransfer
     cursor.execute(f'''
     CREATE TABLE IF NOT EXISTS Users (
     id INTEGER PRIMARY KEY,
     username TEXT NOT NULL,
-    leap TEXT NOT NULL,
+    leap INTEGER NOT NULL,
     list_money TEXT,
     added_money TEXT,
     start_amount INTEGER,
     end_amount INTEGER NOT NULL,
-    initial_date timestamp DEFAULT {x},
+    initial_date timestamp,
     data_transfer timestamp
     )
     ''')
 
     cursor.execute(
         "INSERT into Users (id, username, leap, list_money, added_money, start_amount, end_amount, initial_date, data_transfer) VALUES (?,?,?,?,?,?,?,?,?)",
-        (user, 'test', '1', '100', '0', 0, '66000', f'{x}', f'{x}'))
+        (user, 'test', user_challenge.getLeap(), user_challenge.getListMoney(), user_challenge.getAddedMoney(), user_challenge.getStartAmount(), user_challenge.getEndAmount(), user_challenge.getInitiateDate(), user_challenge.getDateTransfer()))
 
     connection.commit()
     connection.close()
+
+def select_user(user):
+    connection = sqlite3.connect('year_challenge.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute(f"SELECT * FROM Users WHERE username = {user}")
+
+    rows = cursor.fetchall()
+    connection.close()
+    return rows[0]
+
+def update_user(user, user_challenge: YearChallenge):
+    connection = sqlite3.connect('year_challenge.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute(f"UPDATE Users SET * WHERE id = {user}")
 
 if __name__ == "__main__":
     connection = sqlite3.connect('year_challenge.db')
